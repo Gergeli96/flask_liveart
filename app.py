2b55b6a9b8a98233f2e1b8f5e31f0db32db842a0
Nemes Gergely <nemes.gergely96@gmail.com>
2018-10-28 17:25:26 +0100

    registration ang login

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+import pymysql.cursors
+import psycopg2
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
 import os
 from werkzeug import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
-#mysql = MySQL()
 
-# MySQL configurations
-#app.config['MYSQL_DATABASE_USER'] = 'root'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-#app.config['MYSQL_DATABASE_DB'] = 'josi'
-#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-#mysql.init_app(app)
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='josi',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
-#conn = mysql.connect()
-#cursor = conn.cursor()
+conn = psycopg2.connect("dbname=josi user=postgres password=root host=localhost")
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/')
@@ -27,11 +40,10 @@ def home():
 def galery():
     return render_template('galery.html')
 
-#@app.route('/boss')
-#def boss():
-#    return render_template('boss.html')
 
 
+class UploadForm(Form):
+    chategory = StringField('chategory', [validators.Length(min=1, max=50)])
 
 
 UPLOAD_FOLDER = 'static/uploadedImages'
@@ -43,7 +55,9 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/boss', methods=['GET', 'POST'])
+@is_logged_in
 def upload_file():
+    form = UploadForm(request.form)
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -59,12 +73,99 @@ def upload_file():
             filename = secure_filename(file.filename)
             print(filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            category = form.chategory.data
+            cur = conn.cursor()
+            cur.execute("INSERT INTO images (name, category) VALUES (%s, %s)", (filename, category))
+            conn.commit()
+            cur.close()
+
             return redirect(url_for('upload_file', filename=filename))
 
+    return render_template('boss.html', form=form)
 
-    return render_template('boss.html')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+# User Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (name, email, username, password) VALUES (%s, %s, %s, %s)", (name, email, username, password))
+        conn.commit()
+        cur.close()
+
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if cur.rowcount > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            print('adatok')
+            print(data[4])
+            print(password_candidate)
+            password = data[4]
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('upload_file'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+
+# Logout
+@app.route('/logout')
+#@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
 
 
 
 if __name__ == '__main__':
+    app.secret_key='secret123'
     app.run(debug=True)
