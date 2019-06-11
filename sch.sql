e07539d29c2be58915b5199b54463c2c69929175
Nemes Gergely <nemesgergely@pop-os.localdomain>
2019-06-11 20:28:19 +0200

    first commit

--- sch.sql
+++ sch.sql
@@ -4,18 +4,18 @@ create table users (
   name VARCHAR (30),
   email VARCHAR (100),
   username VARCHAR (100),
-  password VARCHAR (100),
+  password VARCHAR (100)
 );
 
-drop table if exists images;
-create table images (
-  id serial PRIMARY KEY,
-  name VARCHAR (210),
-  category VARCHAR (45),
-);
+-- drop table if exists images;
+-- create table images (
+--   id serial PRIMARY KEY,
+--   name VARCHAR (210),
+--   category VARCHAR (45),
+-- );
 
-drop table if exists chategories;
-create table chategories (
-  id serial PRIMARY KEY,
-  category VARCHAR (45)
-);
+-- drop table if exists chategories;
+-- create table chategories (
+--   id serial PRIMARY KEY,
+--   category VARCHAR (45)
+-- );
